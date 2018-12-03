977e815da79a5891c2947626e53ee58640f3a4d7
Marcio Rodrigues Mafideju <mafideju@outlook.com>
2018-12-03 20:51:31 -0200

    DELETE - No Tests, No Heroku, No Advanced Postman

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  return this.update({
+    $pull: {
+      tokens: {
+        token
+      }
+    }
+  })
+}
+
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
@@ -69,6 +79,21 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  return this.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) resolve(user)
+        else reject()
+      })
+    })
+  })
+}
 
 UserSchema.pre('save', function (next) {
   var user = this;
